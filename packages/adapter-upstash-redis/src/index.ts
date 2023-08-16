856ec4eb8c69d4747b29ade7a62f4286f6a5070d
Hadley Lambeth <hlambeth@brikit.com>
2023-08-16 07:55:04 -0700

    add require crypto

--- packages/adapter-upstash-redis/src/index.ts
+++ packages/adapter-upstash-redis/src/index.ts
@@ -69,7 +69,7 @@ export const defaultOptions = {
   userKeyPrefix: "user:",
   verificationTokenKeyPrefix: "user:token:",
 }
-
+const crypto = require('crypto')
 const isoDateRE =
   /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/
 function isDate(value: any) {
